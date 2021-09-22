3de624df4fa5716da04ff23072773b104ab901ae
danielyang <danielyang@tencent.com>
2021-09-22 15:09:20 +0800

    container with most water

--- algorithms/golang/containerWithMostWater/solution.go
+++ algorithms/golang/containerWithMostWater/solution.go
@@ -1,1 +1,33 @@
 package containerWithMostWater
+
+import "math"
+
+func maxArea(height []int) int {
+	l := 0
+	r := len(height) - 1
+	res := math.MinInt32
+
+	for ; l < r ; {
+		res = max(res, min(height[l], height[r]) * (r - l))
+		if height[l] < height[r] {
+			l++
+		} else {
+			r--
+		}
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
--- algorithms/golang/diameterOfBinaryTree/solution.go
+++ algorithms/golang/diameterOfBinaryTree/solution.go
@@ -1,1 +1,43 @@
 package diameterOfBinaryTree
+
+/**
+* Definition for a binary tree node.
+* type TreeNode struct {
+*     Val int
+*     Left *TreeNode
+*     Right *TreeNode
+* }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var res int
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	res = 0
+	helper(root)
+	return res
+}
+
+func helper(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := helper(root.Left)
+	r := helper(root.Right)
+	if l + r > res {
+		res = l + r
+	}
+	return max(r, l) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
